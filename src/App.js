432c2d4018c8487e5fa5345069cfa7faa1ae3877
vedant purohit <vedantpurohit1122@gmail.com>
2024-09-17 00:14:01 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          vedant or lekhraj
+          vedant or to or lekhraj
         </p>
         
         <a
